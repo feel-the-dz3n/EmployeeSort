524d35e886bbf8b096794ebb6aa0ef45aa956d96
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-14 15:03:37 +0200

    Create constructor and getAge method for Employee class

--- src/com/yaroslav/Employee.java
+++ src/com/yaroslav/Employee.java
@@ -1,8 +1,18 @@
 package com.yaroslav;
 
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Employee {
     public String name;
     public LocalDate birthday;
+
+    public Employee(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public int getAge() {
+        return Period.between(birthday, LocalDate.now()).getYears();
+    }
 }
