2a130797b1dd0ad3ce2c4e86a5f13c56a2ab5666
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-14 14:58:46 +0200

    Update sortByBirthdate

--- src/com/yaroslav/EmployeeSort.java
+++ src/com/yaroslav/EmployeeSort.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.HashMap;
 
 public class EmployeeSort {
-    public static HashMap<LocalDate, Employee> sort(Employee[] employees) {
+    // Sorts employees by birthdate
+    // employees: employees array
+    // monthCount: 0 - for current month, 1 - for current and next month, etc..
+    public static HashMap<LocalDate, Employee> sortByBirthdate(Employee[] employees, Integer monthCount) {
         HashMap<LocalDate, Employee> map = new HashMap<>();
 
         return map;
