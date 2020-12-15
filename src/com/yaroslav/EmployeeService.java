83d0f46398084f5bff84aa9c50e6fbd2eb5c6c9b
Yaroslav Kibysh <yanet.prod@gmail.com>
2020-12-16 00:12:10 +0200

    Fix sortByBirthday

--- src/com/yaroslav/EmployeeService.java
+++ src/com/yaroslav/EmployeeService.java
@@ -4,24 +4,29 @@ import java.time.LocalDate;
 import java.time.YearMonth;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 public class EmployeeService {
-    public HashMap<YearMonth, Employee> sortByBirthday(
+    public HashMap<YearMonth, HashSet<Employee>> sortByBirthday(
             Collection<Employee> employees,
             Integer monthCount) {
         return sortByBirthday(employees, monthCount, LocalDate.now());
     }
 
-    public HashMap<YearMonth, Employee> sortByBirthday(
+    public HashMap<YearMonth, HashSet<Employee>> sortByBirthday(
             Collection<Employee> employees,
             Integer monthCount,
             LocalDate relativeDate) {
-        HashMap<YearMonth, Employee> map = new HashMap<>();
+        HashMap<YearMonth, HashSet<Employee>> map = new HashMap<>();
 
         // Iterating through each month
-        for (int i = 0; i < monthCount; i++) {
+        for (int i = 0; i <= monthCount; i++) {
             // Adding current month iteration to relative date
             LocalDate calculatedDate = relativeDate.plusMonths(i);
+
+            // Suitable employees for current y/m will be stored here
+            HashSet<Employee> localEmployees = new HashSet<>();
 
             // Iterating through employees
             for (var employee : employees) {
@@ -31,12 +36,14 @@ public class EmployeeService {
                 // in the same year/month of previously calculated date
                 if (nextBirthday.getYear() == calculatedDate.getYear()
                         && nextBirthday.getMonth() == calculatedDate.getMonth()) {
-                    // OK, so put the information to our hashmap
-                    map.put(
-                            YearMonth.of(calculatedDate.getYear(), calculatedDate.getMonth()),
-                            employee);
+                    // OK, so put the information to our hashset
+                    localEmployees.add(employee);
                 }
             }
+
+            // Store y/m and employees information in our hashmap
+            map.put(YearMonth.of(calculatedDate.getYear(), calculatedDate.getMonth()),
+                    localEmployees);
         }
 
         return map;
